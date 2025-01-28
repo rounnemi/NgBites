ec2a2db0307f3c09b29a1e5f590b1d0d9a54a8c7
oumayma159 <oumaima.elfidha@insat.ucar.tn>
2025-01-28 19:22:01 +0100

    add dashboard link to the header

--- NgBites/src/app/app.routes.ts
+++ NgBites/src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { LoginPageComponent } from './components/pages/login-page/login-page.component';
 import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { ProfilePageComponent } from './components/pages/profile-page/profile-page.component';
 
 export const routes: Routes = [
     // no lazy loading
     { path: 'login', component: LoginPageComponent },
     { path: 'register', component: RegisterPageComponent },
-    {path :'', component: HomeComponent}
+    {path :'', component: HomeComponent},
+    { path: 'dashboard', component: DashboardComponent},
+    { path: 'profile', component: ProfilePageComponent},
 
 ];
