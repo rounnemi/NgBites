07cfdd11ad97dc6b03939c57c2a038484b73a7c5
Nour Soussia <nour.soussia@insat.ucar.tn>
2025-01-28 21:06:21 +0100

    Cart details

---
+++ NgBites/src/app/components/cart-page/cart-page.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { CartService } from '../../services/cart.service';
+import { Cart } from '../../models/Cart';
+import { CartItem } from '../../models/CartItem';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
+
+@Component({
+    selector: 'app-cart-page',
+    templateUrl: './cart-page.component.html',
+    styleUrls: ['./cart-page.component.css'],
+    standalone: true,
+    imports: [ RouterLink, ReactiveFormsModule, CurrencyPipe]
+})
+export class CartPageComponent {
+
+  cart!: Cart;
+
+  constructor(private cartService: CartService)
+  {
+    this.cartService.getCartObservable().subscribe((cart) => {
+      this.cart = cart;
+    })
+  }
+
+  removeFromCart(cartItem: CartItem) {
+    this.cartService.removeFromCart(cartItem.food.id);
+  }
+
+  changeQuantity(cartItem: CartItem, quantityInString: string) {
+    const quantity = parseInt(quantityInString);
+    if(quantity > 0) {
+      this.cartService.changeQuantity(cartItem.food.id, quantity);
+    }
+  }
+
+}
