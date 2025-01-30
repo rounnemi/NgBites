754ba1b1a6b5a23cf25375df7ed25a4b920989de
Nour Soussia <nour.soussia@insat.ucar.tn>
2025-01-30 22:01:10 +0100

    update home page

--- NgBites/src/app/components/pages/food-list/food-list.component.ts
+++ NgBites/src/app/components/pages/food-list/food-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges, Signal, signal } from '@angular/core';
+import { Component, Input, signal, computed } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Food } from '../../../models/Food';
@@ -10,23 +10,6 @@ import { Food } from '../../../models/Food';
   templateUrl: './food-list.component.html',
   styleUrls: ['./food-list.component.css']
 })
-export class FoodListComponent implements OnChanges {
-  @Input() foods: Food[] = [];
-  @Input() itemsPerPage = 10;
-
-  allFoods = signal<Food[]>([]);
-  visibleFoods = signal<Food[]>([]);
-
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['foods'] && this.foods) {
-      console.log("SALURTTT",this.foods);
-      this.allFoods.set(this.foods);
-      this.visibleFoods.set(this.foods.slice(0, this.itemsPerPage));
-    }
-  }
-
-  loadMore() {
-    const nextItems = this.allFoods().slice(this.visibleFoods().length, this.visibleFoods().length + this.itemsPerPage);
-    this.visibleFoods.set([...this.visibleFoods(), ...nextItems]);
-  }
+export class FoodListComponent {
+  @Input() foods: Food[] = []; 
 }
--- NgBites/src/app/components/pages/home/home.component.ts
+++ NgBites/src/app/components/pages/home/home.component.ts
@@ -1,51 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { Observable } from 'rxjs';
 import { FoodService } from '../../../services/food.service';
 import { Food } from '../../../models/Food';
 import { CommonModule } from '@angular/common';
 import { TagsComponent } from '../../shared/tags/tags.component';
 import { SearchComponent } from '../../shared/search/search.component';
 import { FoodListComponent } from '../food-list/food-list.component';
+import { switchMap } from 'rxjs';
+
 @Component({
   selector: 'app-home',
-  imports: [FoodListComponent,SearchComponent,CommonModule, TagsComponent, RouterLink],
+  imports: [FoodListComponent, SearchComponent, CommonModule, TagsComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true
 })
 export class HomeComponent {
-  foods: Food[] = [];
-  todayMenu: Food[] = [];
-  mostOrdered: Food[] = [];
-  visibleFoods: Food[] = [];
-  itemsPerPage = 10; 
+  foods = signal<Food[]>([]);
+  itemsPerPage = 5;
+  page = signal(1);
 
-  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
-    let foodsObservable: Observable<Food[]>;
+  todayMenu = computed(() => this.foods().slice(0, 3));
+  mostOrdered = computed(() => [...this.foods()].sort((a, b) => b.stars - a.stars).slice(0, 5));
+  visibleFoods = computed(() => this.foods().slice(0, this.page() * this.itemsPerPage));
+  constructor(private foodService: FoodService, private activatedRoute: ActivatedRoute) {}
 
-    activatedRoute.params.subscribe((params) => {
-      console.log(params)
-      if (params['searchTerm']) {
-        foodsObservable = this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
-      } else if (params['tag']) {
-        console.log("hgjerkfb")
-        foodsObservable = this.foodService.getAllFoodsByTag(params['tag']);
-      } else {
-        foodsObservable = this.foodService.getAll();
-      }
-
-      foodsObservable.subscribe((serverFoods) => {
-        this.foods = serverFoods;
-        this.todayMenu = serverFoods.slice(0, 3);
-        this.mostOrdered = serverFoods.sort((a, b) => b.stars - a.stars).slice(0, 5);
-        this.visibleFoods = this.foods.slice(0, this.itemsPerPage);
+  ngOnInit() {
+    this.activatedRoute.params
+      .pipe(
+        switchMap((params) => {
+          if (params['searchTerm']) {
+            return this.foodService.getAllFoodsBySearchTerm(params['searchTerm']);
+          } else if (params['tag']) {
+            return this.foodService.getAllFoodsByTag(params['tag']);
+          } else {
+            return this.foodService.getAll();
+          }
+        })
+      )
+      .subscribe((serverFoods) => {
+        this.foods.set(serverFoods);
+        this.page.set(1);
       });
-    });
   }
 
   loadMore() {
-    const nextItems = this.foods.slice(this.visibleFoods.length, this.visibleFoods.length + this.itemsPerPage);
-    this.visibleFoods = [...this.visibleFoods, ...nextItems];
+    this.page.set(this.page() + 1);
   }
 }
